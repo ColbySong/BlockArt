dc89ec2bce352d4c8ccdbbf5b40de1d6a44f1863
dchen121 <dchen121@gmail.com>
2018-02-07 12:08:18 -0800

    Setup ink miner server

--- ink-miner.go
+++ ink-miner.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/x509"
+	"encoding/gob"
+	"encoding/hex"
 	"flag"
 	"fmt"
-	"os"
 	"log"
-	"net/rpc"
 	"net"
+	"net/rpc"
+	"os"
 	"time"
-	"crypto/ecdsa"
-	"encoding/gob"
-	"crypto/elliptic"
-	"encoding/hex"
-	"crypto/x509"
+
 	"./args"
 	"./blockartlib"
 )
@@ -20,23 +21,26 @@ import (
 const HeartbeatMultiplier = 2
 
 type InkMiner struct {
-	addr net.Addr
-	server *rpc.Client
-	pubKey *ecdsa.PublicKey
-	privKey *ecdsa.PrivateKey
+	addr     net.Addr
+	server   *rpc.Client
+	pubKey   *ecdsa.PublicKey
+	privKey  *ecdsa.PrivateKey
 	settings *blockartlib.MinerNetSettings
+}
+
+type MServer struct {
+	inkMiner *InkMiner // TODO: Not sure if MServer needs to know about InkMiner
 }
 
 var (
 	errLog *log.Logger = log.New(os.Stderr, "[miner] ", log.Lshortfile|log.LUTC|log.Lmicroseconds)
-	//outLog *log.Logger = log.New(os.Stderr, "[miner] ", log.Lshortfile|log.LUTC|log.Lmicroseconds)
+	outLog *log.Logger = log.New(os.Stderr, "[miner] ", log.Lshortfile|log.LUTC|log.Lmicroseconds)
 )
 
 // Start the miner.
 func main() {
 	gob.Register(&net.TCPAddr{})
 	gob.Register(&elliptic.CurveParams{})
-
 
 	// Command line input parsing
 	flag.Parse()
@@ -48,20 +52,17 @@ func main() {
 	//pubKey := flag.Arg(1) // do we even need this? follow @367 on piazza
 	privKey := flag.Arg(2)
 
-
 	// Decode keys from strings
 	privKeyBytesRestored, _ := hex.DecodeString(privKey)
 	priv, err := x509.ParseECPrivateKey(privKeyBytesRestored)
 	handleError("Couldn't parse private key", err)
 	pub := priv.PublicKey
 
-
 	// Establish RPC channel to server
 	server, err := rpc.Dial("tcp", serverAddr)
 	handleError("Could not dial server", err)
 	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
 	handleError("Could not resolve miner address", err)
-
 
 	// Create InkMiner instance
 	// todo - replace with TCP listener addr?
@@ -71,9 +72,22 @@ func main() {
 
 	go miner.sendHeartBeats()
 
-	// todo - start listening for RPC calls from art & miner nodes
-}
+	// Start listening for RPC calls from art & miner nodes
+	mserver := new(MServer)
+	mserver.inkMiner = miner
 
+	minerServer := rpc.NewServer()
+	minerServer.Register(mserver)
+
+	inbound, err := net.ListenTCP("tcp", addr)
+	handleError("Listen error", err)
+	outLog.Printf("Server started. Receiving on %s\n", inbound.Addr().String())
+
+	for {
+		conn, _ := inbound.Accept()
+		go minerServer.ServeConn(conn)
+	}
+}
 
 // Registers the miner node on the server by making an RPC call.
 // Returns the miner network settings retrieved from the server.
@@ -87,7 +101,6 @@ func (m InkMiner) register() blockartlib.MinerNetSettings {
 	handleError("Could not register miner", err)
 	return resp
 }
-
 
 // Periodically send heartbeats to the server at period defined by server times a frequency multiplier
 func (m InkMiner) sendHeartBeats() {
